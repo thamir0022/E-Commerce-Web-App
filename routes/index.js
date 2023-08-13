25610b7fa4e18614b93983bc0b25893efaf8efdf
Thamir <thamirsiddik@gmail.com>
2023-08-13 20:57:05 +0530

    Created product template and renderd images.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+let products = require('../public/javascripts/products.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { products });
 });
 
 module.exports = router;
